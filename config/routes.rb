d96b8f7c398f87773d0b7b19128e1a074f8235fa
Sanzhar Musaev <otakusan777@gmail.com>
2015-02-12 16:13:24 +0600

    Implement sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'projects#index'
 
+  resources :users
   resources :projects do
     resources :tickets
   end  
