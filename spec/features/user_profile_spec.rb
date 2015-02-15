f3522d7b8f926e736fd2bf2773ab5a7c75399608
Sanzhar Musaev <otakusan777@gmail.com>
2015-02-15 12:22:55 +0600

    Don't show projects that a user doesn't have permission to see

--- spec/features/user_profile_spec.rb
+++ spec/features/user_profile_spec.rb
@@ -2,23 +2,22 @@ require 'rails_helper'
 require 'spec_helper'
 
 feature "Profile page" do
+	let!(:user) { FactoryGirl.create(:user) }
 
-	scenario "viewing" do
-		user = FactoryGirl.create(:user)
+	before do
+		sign_in_as!(user)
 		visit user_path(user)
+	end					
+
+	scenario "Viewing a user" do
 		expect(page).to have_content(user.name)
 		expect(page).to have_content(user.email)
 	end
-end
 
-feature "Editing Users" do
-	
-	scenario "Updating a project" do
-		user = FactoryGirl.create(:user)
-		visit user_path(user)
+	scenario "Updating a user" do
 		click_link "Edit Profile"
 		fill_in "Username", with: "new_username"
 		click_button "Update Profile"
 		expect(page).to have_content("Profile has been updated.")
-	end
-end				
+	end	
+end
